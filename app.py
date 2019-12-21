5979c9004df4744ea790ffa8e4f63a8a394f0aa6
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-21 16:25:19 +0000

    Add REST API

---
+++ app.py
@@ -0,0 +1,35 @@
+from db import db
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+# turns off Flask SQLAlchemy mod tracker
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# can be postgresql
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.secret_key = 'isaac'
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, "/items")
+api.add_resource(StoreList, "/stores")
+api.add_resource(UserRegister, '/register')
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True)
